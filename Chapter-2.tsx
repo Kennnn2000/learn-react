e613bca60ad35899cd1f6d25ad668ba72f9ee947
Ruo Miyata <miyaty.ruo@gmail.com>
2025-02-05 19:34:14 +0900

    auto save

--- Chapter-2.tsx
+++ Chapter-2.tsx
@@ -8,3 +8,32 @@
 詳細は以下参照
 https://developer.mozilla.org/ja/docs/Web/Manifest
 */
+
+
+// 2-1-2 Npm Scripts
+/*
+npm run startなどのコマンドの正体は、package.jsonのscriptsに記述されているショートカットコマンドでNpm Scriptsと呼ばれる
+例えばnpm run startは、pakage.jsonに"react-scripts start"と記述されているので、実際にはreact-scripts startが実行される
+Npm Scriptsを実行する際は「npm run」を先頭につける必要がある ただしnpm startだけrunを省略することができる
+
+Npm Scriptsを使用することで以下のメリットがある
+・オプションを追加した場合にもコマンドに影響しない
+・利用しているライブラリ/フレームワークを問わずコマンドを統一できる
+
+create-react-appで生成されるpackage.jsonのscriptsは以下の通り
+npm run start
+開発用サーバーを起動
+
+npm run build
+アプリをビルドし、本番環境にデプロイするためのファイル一式を作成できる
+コマンドを実行すると、/buildフォルダにビルドされたファイルが出力されるので、そ
+の配下のファイルをサーバのドキュメントルートに配置することで、アプリを公開できる
+
+npm run test
+テストを実行
+
+npm run eject
+Create React Appを構成するwebpack/Babelの設定をカスタマイズするためのコマンド
+/config、/scriptsフォルダに標準の設定ファイルを出力するので、これを編集する
+詳細は§5-2-1参照
+*/
