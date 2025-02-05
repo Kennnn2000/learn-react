539c0bc4c85874843189a5325fa71eca9d340ad2
Ruo Miyata <miyaty.ruo@gmail.com>
2025-02-05 19:20:14 +0900

    auto save

--- Chapter-2.tsx
+++ Chapter-2.tsx
@@ -1,1 +1,10 @@
 // Chapter-2 Reactの基礎
+
+// manifest.json
+/*
+/publicフォルダ直下にあるmanifest.jsonは、Webページをネイティブアプリとして利用する際に、
+スマホのホーム画面などに表示するアイコンやタイトル、起動時の見た目などを指定するための設定ファイル
+
+詳細は以下参照
+https://developer.mozilla.org/ja/docs/Web/Manifest
+*/
