e715a54134ab3ce30d6c39418944205b6a02b682
Ruo Miyata <miyaty.ruo@gmail.com>
2025-02-05 22:33:50 +0900

    auto save

--- my-react/src/Hooks/useState.tsx
+++ my-react/src/Hooks/useState.tsx
@@ -4,6 +4,7 @@ const [状態, 状態を変更する関数] = useState(初期値);
 状態を変更する関数には引数に値を渡す方法と関数を渡す方法がある 下の例では-ボタンが値を渡し、+ボタンが関数を渡している
 */
 
+import React from 'react';
 import { useState } from 'react';
 
 type CounterProps = {
