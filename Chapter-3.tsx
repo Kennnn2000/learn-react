98dd021e8faf04e4f25b37e00b1f18f80e238926
Ruo Miyata <miyaty.ruo@gmail.com>
2025-02-06 00:16:04 +0900

    auto save

--- Chapter-3.tsx
+++ Chapter-3.tsx
@@ -191,3 +191,52 @@ root.render(<TitledPanel>
   <p key="body">body</p>
 </TitledPanel>
 */
+
+// 3-3-4 型チェック
+/*
+Reactでは以下の3つの方法で型チェックを行うことが多い
+・TypeScriptの型定義
+  割愛
+
+
+・PropTypes
+汎用的な型チェッカー 変数、関数、Reactコンポー年tの等に型情報を追加する手段を提供
+型の不整合はビルド時に通知される
+
+npm install prop-typesでインストール可能
+import PropTypes from 'prop-types'のようにしてインポート
+例えば以下のように表す
+import PropTypes from 'prop-types'
+function MyHello({name}) {
+  return <div>Hello, {name}!</div>
+}
+MyHello.propTypes = { // コンポーネント名.propTypesに対してプロパティ名:型情報の形で型情報を設定
+  name: PropTypes.string.isRequired // isRequiredをつけることで必須項目として設定
+}
+
+export default MyHello // exportの前にPropTypesを設定しなければいけない
+
+他には下記のような型を設定可能
+PropTypes.string: 文字列型
+PropTypes.number: 数値型
+PropTypes.bool: 真偽値型
+PropTypes.array: 配列型 要素の型は任意
+PropTypes.object: オブジェクト型 要素の型は任意
+PropTypes.func: 関数型
+PropTypes.node: element、number、string、arrayのいずれか
+PropTypes.element: React要素
+PropTypes.any: 任意の型
+PropTypes.symbol: シンボル型
+PropTypes.instanceOf(コンポーネント名): 指定したコンポーネントのインスタンスであることを意味する
+PropTypes.oneOf([値1, 値2, ...]): 指定した列挙値のいずれかであることを意味する
+PropTypes.oneOfType([型1, 型2, ...]): 指定した型のいずれかであることを意味する 指定できるのはnumberやstringなどの基本型
+PropTypes.arrayOf(型): 配列内の要素型を指定できる
+PropTypes.objectOf(型): オブジェクト内のプロパティ型を一律に指定できる
+PropTypes.shape({key: 型}): オブジェクトのプロパティ型を個別に指定できる keyにはプロパティ名、型には型情報を指定
+PropTypes.exact({key: 型}): shapeをより厳密にしたもの 使用方法はshapeと同じ shapeは最低限指定されたプロパティのみをチェックするが、exactは指定されたプロパティ以外はエラーとなる
+
+・Flow
+Reactコンポーネントのプロパティに型情報を追加するためのライブラリ
+型の不整合は実行時に通知される
+詳細は割愛
+*/
